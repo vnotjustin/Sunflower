db166e61535ac6d13e418f4485d16c5dc5d8abb9
Justin <justintrongvo@gmail.com>
2019-09-25 12:30:04 -0400

    test

---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public float maxX = 60;
+    public float minX = -60;
+    public float maxY = 360;
+    public float minY = -360;
+    public float sensitivityX = 35f;
+    public float sensitivityY = 35f;
+    public float rotationX = 0f;
+    public float rotationY = 0f;
+    public Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY += Input.GetAxis("Mouse X");
+        rotationX += Input.GetAxis("Mouse Y");
+        rotationX = Mathf.Clamp(rotationX, minX, maxX);
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+
+
+
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    move.transform(epicspeed, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(-epicspeed, 0, 0);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(0, -epicspeed, 0);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(0, epicspeed, 0);
+        //}
+
+    }
+}
---
+++ Assets/Scripts/playMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMove : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam;
+    public CharacterController controller;
+    public float epicspeed = 5;
+    public float jumpSpeed = 5;
+    public float gravity = 4;
+    private Vector3 moveDirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -10)
+        {
+            player.transform.position = new Vector3(0, 1, 0);
+        }
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = cam.transform.TransformDirection(moveDirection);
+            moveDirection *= epicspeed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
